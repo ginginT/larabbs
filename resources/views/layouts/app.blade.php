ae57a4d9a8203468055d2705ca3c37c9618721c9
gingin <X_SJian@163.com>
2019-01-03 15:21:13 +0000

    WYSIWYG Editor

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@
     <title>@yield('title', 'Larabbs') - Laravel 进阶教程</title>
     <!-- Styles -->
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @yield('styles')
 </head>
 <body>
     <div id="app" class="{{ route_class() }}-page">
@@ -21,5 +22,6 @@
     </div>
     <!-- Script -->
     <script src="{{ asset('js/app.js') }}"></script>
+    @yield('scripts')
 </body>
 </html>
--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -1,60 +1,81 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="container">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
 
-<div class="container">
-    <div class="col-md-10 col-md-offset-1">
-        <div class="panel panel-default">
+                <div class="panel-body">
+                    <h2 class="text-center">
+                        <i class="glyphicon glyphicon-edit"></i>
+                        @if($topic->id)
+                            编辑话题
+                        @else
+                            新建话题
+                        @endif
+                    </h2>
 
-            <div class="panel-body">
-                <h2 class="text-center">
-                    <i class="glyphicon glyphicon-edit"></i>
+                    <hr>
+
+                    @include('common.error')
+
                     @if($topic->id)
-                        编辑话题
-                    @else
-                        新建话题
-                    @endif
-                </h2>
+                        <form action="{{ route('topics.update', $topic->id) }}" method="POST" accept-charset="UTF-8">
+                            <input type="hidden" name="_method" value="PUT">
+                            @else
+                                <form action="{{ route('topics.store') }}" method="POST" accept-charset="UTF-8">
+                                    @endif
 
-                <hr>
+                                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-                @include('common.error')
+                                    <div class="form-group">
+                                        <input class="form-control" type="text" name="title"
+                                               value="{{ old('title', $topic->title ) }}" placeholder="请填写标题" required/>
+                                    </div>
 
-                @if($topic->id)
-                    <form action="{{ route('topics.update', $topic->id) }}" method="POST" accept-charset="UTF-8">
-                        <input type="hidden" name="_method" value="PUT">
-                @else
-                    <form action="{{ route('topics.store') }}" method="POST" accept-charset="UTF-8">
-                @endif
+                                    <div class="form-group">
+                                        <select name="category_id" class="form-control" required>
+                                            <option value="" hidden disabled selected>请选择分类</option>
+                                            @foreach($categories as $category)
+                                                <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
 
-                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
-
-                    <div class="form-group">
-                        <input class="form-control" type="text" name="title" value="{{ old('title', $topic->title ) }}" placeholder="请填写标题" required />
-                    </div>
-
-                    <div class="form-group">
-                        <select name="category_id" class="form-control" required>
-                            <option value="" hidden disabled selected>请选择分类</option>
-                            @foreach($categories as $category)
-                                <option value="{{ $category->id }}">{{ $category->name }}</option>
-                            @endforeach
-                        </select>
-                    </div>
-
-                    <div class="form-group">
-                        <textarea name="body" class="form-control" id="editor" rows="3" placeholder="请填写至少三个字符的内容。" required>
+                                    <div class="form-group">
+                        <textarea name="body" class="form-control" id="editor" rows="3" placeholder="请填写至少三个字符的内容。"
+                                  required>
                             {{ old('body', $topic->body) }}
                         </textarea>
-                    </div>
+                                    </div>
 
-                    <div class="well well-sm">
-                            <button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span> 保存</button>
-                    </div>
-                </form>
+                                    <div class="well well-sm">
+                                        <button type="submit" class="btn btn-primary"><span
+                                                    class="glyphicon glyphicon-ok" aria-hidden="true"></span> 保存
+                                        </button>
+                                    </div>
+                                </form>
+                </div>
             </div>
         </div>
     </div>
-</div>
-
 @endsection
+
+@section('styles')
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/simditor.css') }}">
+@stop
+
+@section('scripts')
+    <script type="text/javascript"  src="{{ asset('js/module.js') }}"></script>
+    <script type="text/javascript"  src="{{ asset('js/hotkeys.js') }}"></script>
+    <script type="text/javascript"  src="{{ asset('js/uploader.js') }}"></script>
+    <script type="text/javascript"  src="{{ asset('js/simditor.js') }}"></script>
+
+    <script>
+        $(document).ready(function(){
+            var editor = new Simditor({
+                textarea: $('#editor'),
+            });
+        });
+    </script>
+@stop
