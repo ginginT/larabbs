ac182eb3eec26f1916e338c0e61cdb92e816749b
gingin <X_SJian@163.com>
2019-01-06 15:08:40 +0000

    Users can edit their own topic

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -69,12 +69,29 @@ class TopicsController extends Controller
 		return redirect()->route('topics.show', $topic->id)->with('message', '成功创建话题！');
 	}
 
+    /**
+     * 话题编辑页面
+     *
+     * @param Topic $topic    话题实例
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
 	public function edit(Topic $topic)
 	{
-        $this->authorize('update', $topic);
-		return view('topics.create_and_edit', compact('topic'));
+	    $this->authorize('update', $topic);
+        $categories = Category::all();
+
+		return view('topics.create_and_edit', compact('topic', 'categories'));
 	}
 
+    /**
+     * 处理话题更新
+     *
+     * @param TopicRequest $request
+     * @param Topic $topic
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
 	public function update(TopicRequest $request, Topic $topic)
 	{
 		$this->authorize('update', $topic);
